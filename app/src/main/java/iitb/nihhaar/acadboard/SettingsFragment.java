a2b823bf4a60c70cfdc4e5b569db2db4b4e575f8
Nihhaar <nihhaar@gmail.com>
2016-11-01 18:03:21 +0530

    Realized to do the project on Git. My First Commit.

---
+++ app/src/main/java/iitb/nihhaar/acadboard/SettingsFragment.java
@@ -0,0 +1,45 @@
+package iitb.nihhaar.acadboard;
+
+import android.content.Intent;
+import android.graphics.drawable.RippleDrawable;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.view.View;
+
+import static iitb.nihhaar.acadboard.MainActivity.*;
+
+/**
+ * Created by Nihhaar on 10/31/2016.
+ */
+
+public class SettingsFragment extends PreferenceFragment {
+
+    private SharedPreferences prefs;
+    private SharedPreferences logins;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        Preference button = (Preference)findPreference("logout");
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent login = new Intent(getActivity(),LoginActivity.class);
+                login.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                logins = getActivity().getSharedPreferences(PREFS_FILE,MODE_PRIVATE);
+                SharedPreferences.Editor remLogin = logins.edit();
+                remLogin.clear();
+                remLogin.commit();
+                startActivity(login);
+                getActivity().finish();
+                return true;
+            }
+        });
+    }
+}
