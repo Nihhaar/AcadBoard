4783037df295dd53d0b0921812bbfbe47b5b7859
Nihhaar <nihhaar@gmail.com>
2016-11-02 16:44:55 +0530

    Added Card View using adapter classes and custom card class. Used for displaying evens when clicked on date beautifully!!!

---
+++ app/src/main/java/iitb/nihhaar/acadboard/Card.java
@@ -0,0 +1,25 @@
+package iitb.nihhaar.acadboard;
+
+/**
+ * Created by Nihhaar on 11/2/2016.
+ */
+
+class Card {
+
+        private String line1;
+        private String line2;
+
+        public Card(String line1, String line2) {
+            this.line1 = line1;
+            this.line2 = line2;
+        }
+
+        public String getLine1() {
+            return line1;
+        }
+
+        public String getLine2() {
+            return line2;
+        }
+
+}
--- app/src/main/java/iitb/nihhaar/acadboard/FirstFragment.java
+++ app/src/main/java/iitb/nihhaar/acadboard/FirstFragment.java
@@ -7,9 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class FirstFragment extends Fragment {
+
+    private static final String TAG = "CardListActivity";
+    private CardArrayAdapter cardArrayAdapter;
+    private ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,8 +26,30 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        CalendarView calendarView=(CalendarView)view.findViewById(R.id.calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+               Toast.makeText(getActivity(), ""+dayOfMonth, 0).show();// TODO Auto-generated method stub
+
+            }
+        });
+
+        listView = (ListView) view.findViewById(R.id.card_listView);
+        listView.addHeaderView(new View(getActivity()));
+        listView.addFooterView(new View(getActivity()));
+        cardArrayAdapter = new CardArrayAdapter(getActivity().getApplicationContext(), R.layout.list_item_card);
+        for (int i = 0; i < 5; i++) {
+            Card card = new Card("Card " + (i+1) + " Line 1", "Card " + (i+1) + " Line 2");
+            cardArrayAdapter.add(card);
+        }
+        listView.setAdapter(cardArrayAdapter);
+
+        return view;
     }
 
 }
